02500e2fc562e6290a39c5734e07d214d5b68813
naskokrisi <naskokrisi@abv.bg>
2013-05-13 19:37:40 +0300

    13.05 Unit

---
+++ Unit Test/Unit_Test.h
@@ -0,0 +1,13 @@
+//
+//  Unit_Test.h
+//  Unit Test
+//
+//  Created by Nasko on 5/12/13.
+//  Copyright (c) 2013 Nasko. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Unit_Test : SenTestCase
+
+@end
---
+++ Unit Test/Unit_Test.m
@@ -0,0 +1,32 @@
+//
+//  Unit_Test.m
+//  Unit Test
+//
+//  Created by Nasko on 5/12/13.
+//  Copyright (c) 2013 Nasko. All rights reserved.
+//
+
+#import "Unit_Test.h"
+
+@implementation Unit_Test
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Unit Test");
+}
+
+@end
